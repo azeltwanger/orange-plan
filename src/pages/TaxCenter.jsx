1ad7b337d9b5448300a47d8933864cd9a1e79f17
Austin Zeltwanger <azelt594@gmail.com>
2025-12-04 20:21:44 +0000

    File changes

--- src/pages/TaxCenter.jsx
+++ src/pages/TaxCenter.jsx
@@ -303,9 +303,11 @@ export default function TaxCenter() {
 
   const bulkDeleteTx = useMutation({
     mutationFn: async (ids) => {
-      // Delete sequentially to avoid race conditions
-      for (const id of ids) {
-        await base44.entities.Transaction.delete(id);
+      // Delete in batches of 20 for speed while avoiding overwhelming the API
+      const batchSize = 20;
+      for (let i = 0; i < ids.length; i += batchSize) {
+        const batch = ids.slice(i, i + batchSize);
+        await Promise.all(batch.map(id => base44.entities.Transaction.delete(id)));
       }
       return ids.length;
     },
